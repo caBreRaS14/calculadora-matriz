d7f842c5f76a48d99b07cb7c6c1f9c31a33edd73
caBreRaS14 <97133878+caBreRaS14@users.noreply.github.com>
2024-07-05 23:32:13 -0500

    cambios agregados --bucles para iterar y agregar los elemetos a nuestra mastris falta crear una funcion para que la mastris se vaya mostrando por pantalla

--- codigo.js
+++ codigo.js
@@ -7,54 +7,73 @@ let numeros = [];
 const mostrar = document.querySelector("#solve");
 
 mostrar.addEventListener("click", ()=>{
-    const mos = document.createElement("P");
-    // const textIt = document.createTextNode(input.value);
-    // mos.appendChild(textIt);
-    // aca.appendChild(mos)
+
+    const aca = document.querySelector(".aca");
+    const item = document.createElement("DIV");
+    item.classList.add("mi-clase");
+    aca.appendChild(item);
+
+    const miClase = document.querySelector(".mi-clase");
+
+    const fragmento = document.createDocumentFragment();
+    
+    let k = 0;
+
+    let ResData;
+    
+
     let inputs = document.querySelectorAll('input[type="number"]');
-
-    for (let i = 0; i<12; i++){
-
+    let arrayAns = [[],[],[]];
+    let newArray = [];
+    for(let i = 0; i<inputs.length; i++){
         let input = inputs[i];
-        let res = input.value
-        let res1 = Number(res)
-    
-
-        const mos = document.createElement("P");
-        const textIt = document.createTextNode(res1);
-        mos.appendChild(textIt);
-        aca.appendChild(mos);
-
-        numeros.push(Number(res1));
-        
-        
+        newArray.push(Number(input.value));
     }
 
-    for (let i = 0; i<4; i++){
-
-        let div = numeros[0];
-        let ResDiv = numeros[i]/div;
-
-        let decimal = new Fraction(ResDiv);
-        //asdasdsd
-
-        const mos = document.createElement("P");
-        const textIt = document.createTextNode(decimal);
-        mos.appendChild(textIt);
-        aca.appendChild(mos);
-        let NewArray = [];
-        NewArray.push(Number(decimal));
+    for(let i = 0; i<3;i++){
+        for(let j=0; j<4; j++){
+            arrayAns[i].splice(j,0,newArray[k]);
+            k=k+1;
+        }
     }
+
+    const res = arrayAns[0][0];
+
+    //Primera divicion del primer elemento --falta limitarlo
+    //para cuando el valor sea 0 o 1;
+
+    for (let i = 0; i <4; i++){
+        arrayAns[0][i] = arrayAns[0][i]/res;
+        console.log(arrayAns);
+    }
+
+    //Imprime por pantalla la primera operacion
+
+    for (let i = 0; i <3; i++){
+        for (let j = 0; j <4; j++){
+            ResData = arrayAns[i][j];
+            const Listado = document.createElement("P");
+            Listado.innerHTML = ResData;
+            fragmento.appendChild(Listado);
+        }
+    }
+
+    const OperFirst = (arrayAns[1][0]*(-1));
+
+    for (let i = 0; i <4; i++){
+        ResData = (OperFirst*arrayAns[0][i])+arrayAns[1][i];
+        console.log(ResData);
+    }
+
 
     
 
+    miClase.appendChild(fragmento);
+    
+    console.log(arrayAns);
 
-
-
-    console.log(numeros);
-
+    console.log(newArray);
 
 });
-
 
 
